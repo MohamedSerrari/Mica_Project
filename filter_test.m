8a03886df01b66ac6eba2706ffb3176f2daa8d27
Mohamed.Serrari <mserrari@dixdeder.pedago.ipb.fr>
2019-04-19 12:23:26 +0200

    Spectrogram and BPM

--- filter_test.m
+++ filter_test.m
@@ -61,12 +61,15 @@ s_MWI = filter(h,1,y_squared);
 %% Thresholding
 % Manual thresholding
 findpeaks(s_MWI);
-th = 3.5*10^13;
+%th = 6*10^14;
 %th = max(s_MWI)/sqrt(3);
 
-ind = zeros(1,length(s_MWI));
-ind(s_MWI < th) = 0;
-ind(s_MWI >= th) = 1;
+%th = input("Enter the threshold:",'s')
+answer = inputdlg('Enter the threshold: ','Defining the threshold');
+th = str2num(answer{1});
+
+%ind = zeros(1,length(s_MWI));
+ind = s_MWI > th;
 
 %% Finding Peaks
 
@@ -74,8 +77,6 @@ figure
 hold on
 
 delay = 8;
-last = 500;
-
 y_delayed = [zeros(1,delay) y_filtered];
 
 QRS_only = y_delayed.*[ind zeros(1,delay)]; % This signal contains the QRS complexes only 
@@ -85,13 +86,10 @@ pks(pks < 10^5) = 0; % Another thresholding is needed because findpeaks gives al
 loc_th = find(pks);
 R_locs = loc(loc_th);
 
+last = 1000;
+
 data_plot = y_delayed(1,1:last); 
-
 plot(data_plot);
-
-for R_loc=R_locs
-    plot(R_loc,data_plot(R_loc),'r*'); text(R_loc,data_plot(R_loc),' R ','Color','red','FontSize',14);
-end
 
 for i=1:length(R_locs)
     % inter_pks is the distance between two peaks, it is updated everytime
@@ -116,12 +114,22 @@ for i=1:length(R_locs)
 
     P_loc = P_loc + start - 1; % Correcting P_loc 
     T_loc = T_loc + S_loc - 1; % Correcting T_loc 
-
+    
+    plot(R_locs(i),data_plot(R_locs(i)),'r*'); text(R_locs(i),data_plot(R_locs(i)),' R ','Color','red','FontSize',14);
     plot(Q_loc, Q_val,'r*'); text(Q_loc,Q_val,' Q ','Color','red','FontSize',14);
     plot(S_loc, S_val,'r*'); text(S_loc,S_val,' S ','Color','red','FontSize',14);
     plot(P_loc, P_val,'r*'); text(P_loc,P_val,' P ','Color','red','FontSize',14);
     plot(T_loc, T_val,'r*'); text(T_loc,T_val,' T ','Color','red','FontSize',14);
 end
 
+%% Spectrogram
+ecg_normal_1 = load('ecg_normal_1.mat');
+ecg_VF = load('ecg_VF.mat');
 
+window = 4*60;
+figure; spectrogram(ecg_normal_1.ecg, window);
+figure; spectrogram(ecg_VF.ecg, window)
 
+%% Tachycardia/Bradycardia
+
+rythym = 1/sum(R_locs(2:end) - R_locs(1:end-1))*(length(R_locs) - 1)*60*Fs
